a58abd1f5eaf8b50d9f123a2933a9d149e9f93d0
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2020-07-28 12:55:37 +0200

    Add a SongList landing screen

--- app/src/main/java/com/luismiguelsoto/flayer/activities/SplashScreen.java
+++ app/src/main/java/com/luismiguelsoto/flayer/activities/SplashScreen.java
@@ -2,9 +2,12 @@ package com.luismiguelsoto.flayer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.luismiguelsoto.flayer.R;
+import com.luismiguelsoto.flayer.activities.main.SongsListScreen;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity( new Intent(SplashScreen.this, SongsListScreen.class) );
+            finish();
+        }, 600);
     }
 }
---
+++ app/src/main/java/com/luismiguelsoto/flayer/activities/main/SongsListScreen.java
@@ -0,0 +1,16 @@
+package com.luismiguelsoto.flayer.activities.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.luismiguelsoto.flayer.R;
+
+public class SongsListScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songs_list);
+    }
+}
